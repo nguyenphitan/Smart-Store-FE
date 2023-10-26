d6f474851d53009e6bb922b3dc035e0ab5aec614
nguyenphitan <phitan2001@gmail.com>
2023-10-27 01:24:30 +0700

    create add new product

--- src/components/layout/components/TheAddNewProduct.vue
+++ src/components/layout/components/TheAddNewProduct.vue
@@ -1,30 +1,30 @@
 <template>
     <div id="the-add-new-product">
-        <form>
+        <form @submit="addNewProduct">
             <!-- Left -->
             <div class="product-img">
-                <input type="file" name="product-img">
+                <input required @change="uploadImg" type="file" name="product-img">
                 <div class="show-img">
-                    <!-- <img src="" alt=""> -->
+                    <img src="" alt="image">
                 </div>
             </div>
 
             <!-- Right -->
             <div class="product-info">
                 <p>Name:</p>
-                <input class="product-name" type="text" name="product-name">
+                <input required class="product-name" type="text" name="product-name">
                 <br>
                 <p>Price:</p>
-                <input class="product-price" type="number" name="product-price">
+                <input required class="product-price" type="number" name="product-price">
                 <br>
                 <p>Inventory:</p>
-                <input class="product-quantity" type="number" name="product-quantity">
+                <input required class="product-quantity" type="number" name="product-quantity">
                 <br>
                 <p>Category:</p>
                 <div class="div-category-save">
                     <select class="product-category" name="product-category">
-                        <option value="Air Pods">Air Pods</option>
-                        <option value="Smart Watch">Smart Watch</option>
+                        <option value="1">Air Pods</option>
+                        <option value="2">Smart Watch</option>
                     </select>
                     <button class="save-new-product" type="submit">Save</button>
                 </div>
@@ -34,8 +34,67 @@
 </template>
 
 <script>
+import axios from 'axios';
+// axios.defaults.withCredentials = true;
+// axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
+
 export default {
-    name: 'add-new-product'
+    name: 'add-new-product',
+    methods: {
+        
+        // Upload img
+        uploadImg() {
+            let imgFile = document.querySelector("#the-add-new-product input[name='product-img']").files[0];
+            let imgUrl = URL.createObjectURL(imgFile);
+            let showImg = document.querySelector("#the-add-new-product .show-img img");
+            showImg.src = imgUrl;
+        },
+
+        // Add new product
+        addNewProduct() {
+            let productName = document.querySelector('#the-add-new-product .product-name').value;
+            productName = encodeURIComponent(productName);
+            let productPrice = document.querySelector('#the-add-new-product .product-price').value;
+            let productQuantity = document.querySelector('#the-add-new-product .product-quantity').value;
+            let categoryId = document.querySelector('#the-add-new-product .product-category').value;
+            let imgFile = document.querySelector("#the-add-new-product input[name='product-img']").files[0];
+            // let formData = new FormData();
+            // formData.append("file", imgFile);
+            
+            // console.log(productName);
+            // console.log(productPrice);
+            // console.log(productQuantity);
+            // console.log(categoryId);
+            // console.log(imgFile);
+
+            const formData = new FormData();
+            formData.append('name', productName);
+            formData.append('price', productPrice);
+            formData.append('quantity', productQuantity);
+            formData.append('categoryId', categoryId);
+            formData.append('image', imgFile);
+
+            // call api:
+            axios
+                .post('http://localhost:8080/admin/api/v1/products', formData, {
+                    headers: {
+                        'Content-Type': 'multipart/form-data'
+                    }
+                })
+                .then((response) => {
+                    console.log(response);
+                    window.location.href = "/";
+                })
+                .catch((reject) => {
+                    console.log(reject);
+                    alert("Fail to add new product!");
+                });
+
+
+
+        },
+
+    }
 }
 </script>
 
