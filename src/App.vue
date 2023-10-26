e27fb951ea31b94f27c8be35a5b95cf0a118f730
nguyenphitan <phitan2001@gmail.com>
2023-10-26 22:34:32 +0700

    update router

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
     <the-header></the-header>
+    <the-login :shopName="shopName"></the-login>
+    <the-register :shopName="shopName"></the-register>
     <router-view></router-view>
 
   </div>
@@ -8,11 +10,26 @@
 
 <script>
 import TheHeader from './components/layout/components/TheHeader.vue'
+import TheLogin from './components/layout/components/TheLogin.vue'
+import TheRegister from './components/layout/components/TheRegister.vue'
 
 export default {
   name: 'App',
   components: {
       TheHeader,
+      TheLogin,
+      TheRegister,
+  },
+  data() {
+    return {
+    shopName: "Ecommerce",
+    buttonName: "Đăng nhập",
+    iconClass: 'fa-brands fa-facebook',
+    textType: 'text',
+    inputPlaceholder: 'example@mail.com',
+    carouselData: [require('@/assets/imgs/ef.jpg'), require('@/assets/imgs/ef.jpg'), require('@/assets/imgs/ef.jpg')],
+    
+    }
   },
 }
 </script>
