de8a8649a3e4e0b1c15c27b7918f325aabdd5e65
nguyenphitan <phitan2001@gmail.com>
2023-11-10 00:34:12 +0700

    delete product out cart

--- src/components/layout/components/TheCart.vue
+++ src/components/layout/components/TheCart.vue
@@ -11,14 +11,18 @@
                                     <div class="row">
                                         <!-- Begin cart left -->
                                         <div class="col-lg-7">
-                                            <h5 class="mb-3"><a href="#!" class="text-body"><i
-                                                class="fas fa-long-arrow-alt-left me-2"></i>Continue shopping</a></h5>
+                                            <h5 class="mb-3">
+                                                <a href="/" class="text-body">
+                                                    <i class="fas fa-long-arrow-alt-left me-2"></i>
+                                                    Continue shopping
+                                                </a>
+                                            </h5>
                                             <hr>
 
                                             <div class="d-flex justify-content-between align-items-center mb-4">
                                             <div>
                                                 <p class="mb-1">Shopping cart</p>
-                                                <p class="mb-0">You have 4 items in your cart</p>
+                                                <p class="mb-0">You have {{ cartData.length }} items in your cart</p>
                                             </div>
                                             <div>
                                                 <p class="mb-0"><span class="text-muted">Sort by:</span> <a href="#!"
@@ -32,33 +36,40 @@
                                                 v-for="(data, key) in cartData" 
                                                 :key="key"
                                             >
-                                                <div class="card-body">
-                                                    <div class="d-flex justify-content-between">
-                                                        <div class="d-flex flex-row align-items-center">
-                                                            <div>
-                                                                <img
-                                                                    v-bind:src="require('@/assets/imgs/' + data.product.photos)"
-                                                                    class="img-fluid rounded-3" alt="Shopping item" style="width: 100px;">
-                                                            </div>
-                                                            <div class="ms-3">
-                                                                <h5 style="font-weight: bold;">{{ data.product.name }}</h5>
-                                                                <div style="display: flex; justify-content: space-between; width: 200px;">
-                                                                    <p class="small mb-0">Price: {{ data.product.price }}</p>
-                                                                    <p class="small mb-0">Quantity: {{ data.quantity }}</p>
+                                                <router-link :to="{ name: 'details', params: { id: data.product.id}}">
+                                                    <div class="card-body">
+                                                        <div class="d-flex justify-content-between">
+                                                            <div class="d-flex flex-row align-items-center">
+                                                                <div>
+                                                                    <img
+                                                                        v-bind:src="require('@/assets/imgs/' + data.product.photos)"
+                                                                        class="img-fluid rounded-3" alt="Shopping item" style="width: 100px;">
+                                                                </div>
+                                                                <div class="ms-3">
+                                                                    <h5 style="font-weight: bold;">{{ data.product.name }}</h5>
+                                                                    <div style="display: flex; justify-content: space-between; width: 200px;">
+                                                                        <p style="font-weight: 600;" class="small mb-0">{{ data.categoryName }}</p>
+                                                                    </div>
+                                                                    <div style="display: flex; justify-content: space-between; width: 200px;">
+                                                                        <p class="small mb-0">Price: {{ data.product.price }}</p>
+                                                                        <p class="small mb-0">Quantity: {{ data.quantity }}</p>
+                                                                    </div>
                                                                 </div>
                                                             </div>
-                                                        </div>
-                                                        <div class="d-flex flex-row align-items-center">
-                                                            <!-- <div style="width: 50px;">
-                                                                <h5 class="fw-normal mb-0">2</h5>
-                                                            </div> -->
-                                                            <div style="width: 120px;">
-                                                                <span class="mb-0">{{ formatPrice(data.product.price * data.quantity) }} VND</span>
+                                                            <div class="d-flex flex-row align-items-center">
+                                                                <!-- <div style="width: 50px;">
+                                                                    <h5 class="fw-normal mb-0">2</h5>
+                                                                </div> -->
+                                                                <div style="width: 120px;">
+                                                                    <span class="mb-0">{{ formatPrice(data.product.price * data.quantity) }} VND</span>
+                                                                </div>
+                                                                <span class="btn-delete-product-out-cart" @click="deleteProduct($event, data.id)" style="color: #cecece; cursor: pointer; padding: 4px;">
+                                                                    <i class="fas fa-trash-alt"></i>
+                                                                </span>
                                                             </div>
-                                                            <a href="#!" style="color: #cecece;"><i class="fas fa-trash-alt"></i></a>
                                                         </div>
                                                     </div>
-                                                </div>
+                                                </router-link>
                                             </div>
                                             <!-- End product row -->
 
@@ -75,13 +86,18 @@
                                                     </div>
 
                                                     <p class="small mb-2">Card type</p>
-                                                    <a href="#!" type="submit" class="text-white"><i
-                                                        class="fab fa-cc-mastercard fa-2x me-2"></i></a>
-                                                    <a href="#!" type="submit" class="text-white"><i
-                                                        class="fab fa-cc-visa fa-2x me-2"></i></a>
-                                                    <a href="#!" type="submit" class="text-white"><i
-                                                        class="fab fa-cc-amex fa-2x me-2"></i></a>
-                                                    <a href="#!" type="submit" class="text-white"><i class="fab fa-cc-paypal fa-2x"></i></a>
+                                                    <a href="#!" type="submit" class="text-white">
+                                                        <i class="fab fa-cc-mastercard fa-2x me-2"></i>
+                                                    </a>
+                                                    <a style="margin: 0 4px;" href="#!" type="submit" class="text-white">
+                                                        <i class="fab fa-cc-visa fa-2x me-2"></i>
+                                                    </a>
+                                                    <a href="#!" type="submit" class="text-white">
+                                                        <i class="fab fa-cc-amex fa-2x me-2"></i>
+                                                    </a>
+                                                    <a style="margin: 0 4px;" href="#!" type="submit" class="text-white">
+                                                        <i class="fab fa-cc-paypal fa-2x"></i>
+                                                    </a>
 
                                                     <form class="mt-4">
                                                     <div class="form-outline form-white mb-4">
@@ -118,22 +134,22 @@
 
                                                     <div class="d-flex justify-content-between">
                                                     <p class="mb-2">Subtotal</p>
-                                                    <p class="mb-2">$4798.00</p>
+                                                    <p class="mb-2">{{ formatPrice(totalPrice) }} VND</p>
                                                     </div>
 
                                                     <div class="d-flex justify-content-between">
                                                     <p class="mb-2">Shipping</p>
-                                                    <p class="mb-2">$20.00</p>
+                                                    <p class="mb-2">{{ formatPrice(20000) }} VND</p>
                                                     </div>
 
                                                     <div class="d-flex justify-content-between mb-4">
                                                     <p class="mb-2">Total(Incl. taxes)</p>
-                                                    <p class="mb-2">$4818.00</p>
+                                                    <p class="mb-2">{{ formatPrice(Number(totalPrice) + 20000) }} VND</p>
                                                     </div>
 
                                                     <button type="button" class="btn btn-info btn-block btn-lg" style="background-color: rgb(233, 176, 69); border: none;">
                                                     <div class="d-flex justify-content-between">
-                                                        <span>$4818.00</span>
+                                                        <span>{{ formatPrice(Number(totalPrice) + 20000) }} VND</span>
                                                         <span>Checkout <i class="fas fa-long-arrow-alt-right ms-2"></i></span>
                                                     </div>
                                                     </button>
@@ -175,6 +191,11 @@ export default {
                 console.log("Get cart detail success!");
                 console.log(response.data);
                 this.cartData = response.data;
+
+                for(let cartResponse of response.data) {
+                    this.totalPrice += (cartResponse.product.price * cartResponse.quantity);
+                }
+
             })
             .catch((reject) => {
                 console.log(reject);
@@ -185,6 +206,9 @@ export default {
             // List product in cart:
             cartData: [],
 
+            // Total price:
+            totalPrice: 0,
+
         }
     },
     methods: {
@@ -192,6 +216,29 @@ export default {
         formatPrice(value) {
             let val = (value/1).toFixed(0).replace('.', ',')
             return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".")
+        },
+
+        // Delete product out to cart:
+        deleteProduct(e, productId) {
+            e.preventDefault();
+            console.log(productId);
+            const token = localStorage.getItem('token');
+
+            // header
+            const headers = {
+                Authorization: `Bearer ${token}`,
+            };
+
+            axios
+                .delete(`http://localhost:8080/api/v1/cart/${productId}`, {headers})
+                .then((response) => {
+                    console.log(response);
+                    console.log('Delete product success!');
+                    window.location.reload();
+                })
+                .catch((reject) => {
+                    console.log(reject);
+                });
         },
     },
 
