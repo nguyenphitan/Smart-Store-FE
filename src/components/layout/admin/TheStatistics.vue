f3a524153f8a6642b64908ad10c8fa1baa507767
nguyenphitan <phitan2001@gmail.com>
2023-12-06 00:16:19 +0700

    complete statistics

--- src/components/layout/admin/TheStatistics.vue
+++ src/components/layout/admin/TheStatistics.vue
@@ -7,44 +7,67 @@
         <!-- Container -->
         <div id="statistics-container">
             <div id="list-month">
-                <div @click="statisticsOfMonth" class="t-month-common month-0 t-active">{{ currentYear }}</div>
-                <div @click="statisticsOfMonth" class="t-month-common month-1">January</div>
-                <div @click="statisticsOfMonth" class="t-month-common month-2">February</div>
-                <div @click="statisticsOfMonth" class="t-month-common month-3">March</div>
-                <div @click="statisticsOfMonth" class="t-month-common month-4">April</div>
-                <div @click="statisticsOfMonth" class="t-month-common month-5">May</div>
-                <div @click="statisticsOfMonth" class="t-month-common month-6">June</div>
-                <div @click="statisticsOfMonth" class="t-month-common month-7">July</div>
-                <div @click="statisticsOfMonth" class="t-month-common month-8">August</div>
-                <div @click="statisticsOfMonth" class="t-month-common month-9">September</div>
-                <div @click="statisticsOfMonth" class="t-month-common month-10">October</div>
-                <div @click="statisticsOfMonth" class="t-month-common month-11">November</div>
-                <div @click="statisticsOfMonth" class="t-month-common month-12">December</div>
+                <div @click="statisticsOfYear($event, currentYear)" class="t-month-common month-0 t-active">{{ currentYear }}</div>
+                <div @click="statisticsOfMonth" tan="1" class="t-month-common month-1">January</div>
+                <div @click="statisticsOfMonth" tan="2" class="t-month-common month-2">February</div>
+                <div @click="statisticsOfMonth" tan="3" class="t-month-common month-3">March</div>
+                <div @click="statisticsOfMonth" tan="4" class="t-month-common month-4">April</div>
+                <div @click="statisticsOfMonth" tan="5" class="t-month-common month-5">May</div>
+                <div @click="statisticsOfMonth" tan="6" class="t-month-common month-6">June</div>
+                <div @click="statisticsOfMonth" tan="7" class="t-month-common month-7">July</div>
+                <div @click="statisticsOfMonth" tan="8" class="t-month-common month-8">August</div>
+                <div @click="statisticsOfMonth" tan="9" class="t-month-common month-9">September</div>
+                <div @click="statisticsOfMonth" tan="10" class="t-month-common month-10">October</div>
+                <div @click="statisticsOfMonth" tan="11" class="t-month-common month-11">November</div>
+                <div @click="statisticsOfMonth" tan="12" class="t-month-common month-12">December</div>
             </div>
-            <Bar
-                id="my-chart-id"
-                :options="chartOptions"
-                :data="chartData"
-            />
+            <apex-chart id="my-chart-id" 
+                type="area" 
+                :options="chartOptions" 
+                :series="series"
+            >
+            </apex-chart>
         </div>
         <!-- End Container -->
     </div>
 </template>
 
 <script>
-import { Bar } from 'vue-chartjs'
-import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
 import axios from 'axios'
-
-ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
 
 export default {
     name: 'the-statistics',
     components: {
-        Bar,
     },
     created() {
-        this.statisticsOfYear(2023);
+        let year = 2023;
+        let me = this;
+        const token = localStorage.getItem('token');
+        // header
+        const headers = {
+            Authorization: `Bearer ${token}`,
+        };
+        // Call API
+        axios
+            .get(`http://localhost:8080/api/v1/statistics/${year}`, { headers })
+            .then((response) => {
+                console.log("Statistics success");
+                let result = [];
+                for(let dto of response.data) {
+                    result.push(dto.total);
+                }
+                // console.log(result);
+
+                // set chart data
+                me.series = [{
+                    name: 'series-1',
+                    data: result
+                }]
+            })
+            .catch((reject) => {
+                console.log("Statistics fail");
+                console.log(reject);
+            });
     },
     data() {
         return {
@@ -52,20 +75,44 @@ export default {
             currentYear: 2023,
             
             // chart data
-            chartData: {
-                labels: [ 'January', 'February', 'March', 'April', 'May', 'June', 'July', 
-                'August', 'September', 'October', 'November', 'December' ],
-                datasets: [ { data: [] } ]
-            },
             chartOptions: {
-                responsive: true
-            }
+                chart: {
+                    id: 'vuechart-example',
+                },
+                xaxis: {
+                    categories: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 
+                'August', 'September', 'October', 'November', 'December']
+                },
+                yaxis: {
+                    labels: {
+                        formatter: function (value) {
+                            let val = (value/1).toFixed(0).replace('.', ',');
+                            return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".") + ' VND';
+                        }
+                    },
+                },
+            },
+            series: [{
+                name: 'series-1',
+                data: [30, 40, 35, 50, 49, 60, 70, 91, 30, 40, 35, 50]
+            }],
         }
     },
     methods: {
+        // Active onclick
+        activeElement(e) {
+            let months = document.querySelectorAll('#the-statistics .t-month-common');
+            for(let month of months) {
+                month.classList.remove('t-active');
+            }
+
+            e.target.classList.add('t-active');
+        },
+
         // Get statistics of the year
-        statisticsOfYear(year) {
+        statisticsOfYear(e, year) {
             let me = this;
+            me.activeElement(e);
             const token = localStorage.getItem('token');
             // header
             const headers = {
@@ -76,21 +123,79 @@ export default {
                 .get(`http://localhost:8080/api/v1/statistics/${year}`, { headers })
                 .then((response) => {
                     console.log("Statistics success");
-                    // let result = [];
+                    let result = [];
                     for(let dto of response.data) {
-                        me.chartData.datasets[0].data.push(dto.total);
+                        result.push(dto.total);
                     }
+                    // console.log(result);
+
+                    // set chart data
+                    me.series = [{
+                        name: 'series-1',
+                        data: result
+                    }]
                 })
                 .catch((reject) => {
+                    console.log("Statistics fail");
                     console.log(reject);
                 });
         },
 
         // Get statistics of month:
         statisticsOfMonth(e) {
-            let month = e.target.innerText;
+            let me = this;
+            me.activeElement(e);
+            let year = 2023;
+            let month = Number(e.target.getAttribute('tan'));
             console.log(month);
+
+            // Call API
+            axios
+                .get(`http://localhost:8080/api/v1/statistics/${year}/${month}`)
+                .then((response) => {
+                    console.log("Statistics success");
+                    let listDate = [];
+                    let result = [];
+                    for(let dto of response.data) {
+                        listDate.push(dto.date);
+                        result.push(dto.total);
+                    }
+                    // console.log(result);
+
+                    // Set chart data
+                    me.chartOptions = {
+                        chart: {
+                            id: 'vuechart-example',
+                        },
+                        xaxis: {
+                            categories: listDate
+                        },
+                        yaxis: {
+                            labels: {
+                                formatter: function (value) {
+                                    let val = (value/1).toFixed(0).replace('.', ',');
+                                    return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".") + ' VND';
+                                }
+                            },
+                        },
+                    },
+
+                    me.series = [{
+                        name: 'series-1',
+                        data: result
+                    }]
+                })
+                .catch((reject) => {
+                    console.log("Statistics fail");
+                    console.log(reject);
+                });
             
+        },
+
+        // Format price:
+        formatPrice(value) {
+            let val = (value/1).toFixed(0).replace('.', ',')
+            return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".")
         },
     },
             
@@ -120,6 +225,7 @@ export default {
     padding: 12px;
     box-shadow: rgba(0, 0, 0, 0.05) 0px 6px 26px;
     border-radius: 8px;
+    height: fit-content;
 }
 
 .t-month-common {
@@ -144,5 +250,6 @@ export default {
 /* Chart */
 #my-chart-id {
     width: 80% !important;
+    margin-top: 40px;
 }
 </style>
